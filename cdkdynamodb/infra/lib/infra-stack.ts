84943de9004f7c16fd956ca6cf000fdae0f4bfbd
Dhruv Vohra <dhruvohra@gmail.com>
2023-12-24 13:19:48 +0530

    Added the name of the dynamodb Table

--- cdkdynamodb/infra/lib/infra-stack.ts
+++ cdkdynamodb/infra/lib/infra-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { Duration } from 'aws-cdk-lib';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
@@ -14,7 +16,7 @@ export class InfraStack extends cdk.Stack {
       },
       readCapacity: 3,
       writeCapacity: 3,
+      tableName: 'MyDynamoDbCDKTestTable',
     })
-
   }
 }
