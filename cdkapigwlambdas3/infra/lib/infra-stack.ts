1e867ef24833aec79eb0037f2433ad295a67f91e
Dhruv Vohra <dhruvohra@gmail.com>
2024-01-01 15:42:37 +0530

    created an iam role for accessing the the lambda

--- cdkapigwlambdas3/infra/lib/infra-stack.ts
+++ cdkapigwlambdas3/infra/lib/infra-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam'
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,5 +9,12 @@ export class InfraStack extends cdk.Stack {
       const bankbucket = new s3.Bucket(this, 'bankbucket', {
         bucketName: 'bankbucket-007'
       })
+
+      const bankrole = new iam.Role(this, 'bankrole', {
+        roleName: 'bankingLambdaRole',
+        description: 'role for lambda to access the s3bucket',
+        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'), 
+      })
+      bankrole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'));
   }
 }
